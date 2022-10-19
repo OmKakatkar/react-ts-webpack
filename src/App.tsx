98089bdc2ef2c9b6cebbf75bef0fd74028ff6c04
Om Kakatkar <omkakatkar@gmail.com>
2022-10-19 08:57:55 +0530

    [wip] Add Image Loaders

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
 import './App.css';
+import IMAGE from './react.png';
+
 export function App() {
-  return <h1>React TypeScript Template</h1>;
+  return (
+    <>
+      <h1>React TypeScript Template</h1>;
+      <img
+        src={IMAGE}
+        alt='React Logo'
+        width='350'
+        height='300'
+      />
+    </>
+  );
 }
